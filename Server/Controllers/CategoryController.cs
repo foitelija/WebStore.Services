a7d77c32fcdff1b5e18fbf2d9e009d0d8d7aaf41
foitelija <foitelija777@gmail.com>
2022-05-06 12:32:22 +0300

    feat: add ProductList and created API CategoryController file

---
+++ Server/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using BlazorShop.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorShop.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        public async Task<ActionResult<List<Category>>> GetCategories()
+        {
+            return Ok();
+        }
+    }
+}
