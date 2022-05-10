bffd363de34e3e0fac0eb1184b5ef68f4049efe5
foitelija <foitelija777@gmail.com>
2022-05-10 20:28:47 +0300

    feat: division into categories, new page

---
+++ Client/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,45 @@
+﻿using BlazorShop.Shared;
+using System.Collections.Generic;
+
+namespace BlazorShop.Client.Services.CategoryService
+{
+    public class CategoryService : ICategoryService
+    {
+        public List<Category> Categories { get; set; } = new List<Category>();
+
+        public void LoadCategories()
+        {
+            Categories = new List<Category>
+            {
+                new Category
+                {
+                    Id = 1,
+                    Name = "Action",
+                    Url = "action-games",
+                    Icon = "aperture",
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name ="Role play",
+                    Url ="role-play-games",
+                    Icon ="aperture",
+                },
+                new Category
+                {
+                    Id=3,
+                    Name ="Fight",
+                    Url="fighting-games",
+                    Icon ="aperture",
+                },
+                new Category
+                {
+                    Id=4,
+                    Name="Race",
+                    Url="racing-games",
+                    Icon="aperture",
+                },
+            };
+        }
+    }
+}
---
+++ Client/Services/CategoryService/ICategoryService.cs
@@ -0,0 +1,10 @@
+﻿using BlazorShop.Shared;
+using System.Collections.Generic;
+namespace BlazorShop.Client.Services.CategoryService
+{
+    public interface ICategoryService
+    {
+        public List<Category> Categories { get; set; }
+        void LoadCategories();
+    }
+}
