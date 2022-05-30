2c235929470f2d583c52ee4d35ba39a24e4e4e3a
Domplas1 <105455213+Domplas1@users.noreply.github.com>
2022-05-30 09:40:36 +0300

    startup config for jwt

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -1,3 +1,4 @@
+using BlazorShop.Server;
 using BlazorShop.Server.Data;
 using BlazorShop.Server.Services.CategoryService;
 using BlazorShop.Server.Services.ProductService;
@@ -9,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using System.Linq;
 using BlazorShop.Server.Services.PaymentService;
 
@@ -33,9 +36,25 @@ namespace BlazorShop.Server
             });
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddTransient<IUserDatabase, UserDatabase>();
             services.AddScoped<ICategoryService, CategoryService>();
             services.AddScoped<IProductService, ProductService>();
             services.AddScoped<IPaymentService, PaymentService>();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = true,
+                    ValidAudience = "http://babidzhonych.herokuapp.com",
+                    ValidateIssuer = true,
+                    ValidIssuer = "http://babidzhonych.herokuapp.com",
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    RequireExpirationTime = false,
+                    ClockSkew = System.TimeSpan.Zero,
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("e25788c2-3e02-4121-9b98-6fd65cb3d566")) // NOTE: THIS SHOULD BE A SECRET KEY NOT TO BE SHARED; A GUID IS RECOMMENDE
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,9 +75,9 @@ namespace BlazorShop.Server
             app.UseHttpsRedirection();
             app.UseBlazorFrameworkFiles();
             app.UseStaticFiles();
-
+            app.UseAuthentication();// NOTE: line is newly added
             app.UseRouting();
-
+            app.UseAuthorization(); // NOTE: line is newly addded, notice placement after UseRouting()
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
